97c2f0a11d9d50600be768519615c912de1073d6
Yasushi Abe <yasushi.abe@gmail.com>
2010-07-30 14:16:01 +0900

    update Sage.

--- project/build/project.scala
+++ project/build/project.scala
@@ -6,7 +6,7 @@ class DiivaacRank(info: ProjectInfo) extends AppengineProject(info) with JRebel 
 
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test"
 
-  val sage = "nkpart" % "sage_2.8.0.RC7" % "0.1"
+  val sage = "nkpart" %% "sage" % "0.1"
   val scalaToolsSnapshot = ScalaToolsSnapshots
 
   override def jrebelJvmOptions =

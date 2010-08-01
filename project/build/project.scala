51f196333bb24d18603baa36e61be01cbd8cda46
Yasushi Abe <yasushi.abe@gmail.com>
2010-08-01 20:02:37 +0900

    add slf4j

--- project/build/project.scala
+++ project/build/project.scala
@@ -2,6 +2,7 @@ import sbt._
 
 class DiivaacRank(info: ProjectInfo) extends AppengineProject(info) with JRebel {
   val tagsoup = "org.ccil.cowan.tagsoup" % "tagsoup" % "1.2"
+  val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.6.1"
   val labs = AppengineApiLabsJar
 
   val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test"

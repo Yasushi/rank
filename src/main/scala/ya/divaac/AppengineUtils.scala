f56a219c418c44ad9fb6f0439359df4150bb5b30
Yasushi Abe <yasushi.abe@gmail.com>
2010-08-05 16:12:18 +0900

    fix memocache memoize.

--- src/main/scala/ya/divaac/AppengineUtils.scala
+++ src/main/scala/ya/divaac/AppengineUtils.scala
@@ -42,13 +42,21 @@ object AppengineUtils {
 
   object Memcache {
     val memcacheService = MemcacheServiceFactory.getMemcacheService
+    import Expiration._
+    val defaultExpire = byDeltaSeconds(3600)
 
+    def validValue[R](value: R) = value != null && (value match {
+      case s: String => !s.isEmpty
+      case s: Seq[_] => !s.isEmpty
+      case s: Array[_] => !s.isEmpty
+      case _ => true
+    })
     class Memoize1[-T, +R](f: T => R, expire: Expiration) extends (T => R) {
       def apply(x: T): R ={
         Option(memcacheService.get(x)) match {
           case None => {
             val value = f(x)
-            if (value != null)
+            if (validValue(value))
               memcacheService.put(x, value, expire)
             value
           }
@@ -57,11 +65,28 @@ object AppengineUtils {
       }
     }
     object Memoize1 {
-      import Expiration._
-      val defaultExpire = byDeltaSeconds(3600)
       def apply[T, R](f: T => R, expire: Expiration) = new Memoize1(f, expire)
       def apply[T, R](f: T => R, expire: Int) = new Memoize1(f, byDeltaSeconds(expire))
       def apply[T, R](f: T => R) = new Memoize1(f, defaultExpire)
     }
+    class Memoize0[-T, +R](f: => R, key: T, expire: Expiration) extends (() => R) {
+      def apply(): R ={
+        Option(memcacheService.get(key)) match {
+          case None => {
+            val value = f
+            if (validValue(value))
+              memcacheService.put(key, value, expire)
+            value
+          }
+          case Some(value) => value.asInstanceOf[R]
+        }
+      }
+    }
+    object Memoize0 {
+      def apply[T, R](f: => R, key: T, expire: Expiration) = new Memoize0(f, key, expire)
+      def apply[T, R](f: => R, key: T, expire: Int) = new Memoize0(f, key, byDeltaSeconds(expire))
+      def apply[T, R](f: => R, key: T) = new Memoize0(f, key, defaultExpire)
+    }
+
   }
 }

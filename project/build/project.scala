3c14d149237803ae7eb4f1a828116a314d681279
Yasushi Abe <yasushi.abe@gmail.com>
2010-08-09 18:29:46 +0900

    add scalacheck

--- project/build/project.scala
+++ project/build/project.scala
@@ -5,7 +5,8 @@ class DiivaacRank(info: ProjectInfo) extends AppengineProject(info) with JRebel 
   val slf4j = "org.slf4j" % "slf4j-jdk14" % "1.6.1"
   val labs = AppengineApiLabsJar
 
-  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5-SNAPSHOT" % "test"
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test"
+  val scalacheck = "org.scala-tools.testing" %% "scalacheck" % "1.7" % "test"
 
   val sage = "nkpart" %% "sage" % "0.1"
   val scalaToolsSnapshot = ScalaToolsSnapshots

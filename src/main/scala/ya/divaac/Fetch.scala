ec4a1ae31d369c1019db0c563a61fef1c9ec0a66
Yasushi Abe <yasushi.abe@gmail.com>
2010-08-10 00:01:09 +0900

    fetch servlet.

--- src/main/scala/ya/divaac/Fetch.scala
+++ src/main/scala/ya/divaac/Fetch.scala
@@ -4,9 +4,7 @@ import javax.servlet.http._
 import com.google.appengine.api.labs.taskqueue._
 
 class Fetch extends HttpServlet {
-  import DivaacRank._
-  import Util._
-  def src(s: String) = memo(s, 3600)((buildURL _) andThen fetch)
+  import DivaacRank2._
 
   val headers = Array("X-AppEngine-QueueName",
                       "X-AppEngine-TaskName",
@@ -14,34 +12,39 @@ class Fetch extends HttpServlet {
 
   def queueAllFetching {
     import TaskOptions.Builder._
-    val index = memo("index", 3600 * 23)(_ => fetch(indexURL))
     val queue = QueueFactory.getQueue("fetch")
-    for (no <- parseIndex(index); diff <- List("hard", "extreme")) {
-      val target = format("/f/%s/%s", no, diff)
+    for (songKey <- fetchSongKeys()) {
+      val target = format("/f/%s", songKey)
       log("queued " + target)
       queue.add(url(target).method(TaskOptions.Method.GET))
     }
   }
 
-  def fetchRanking(songNo: String, difficulty: String, force: Boolean = false) {
-    val paramString = format("songNo: %s, difficulty: %s, force: %s", songNo, difficulty, force)
-    parse(src(songNo + "_" + difficulty)) match {
-      case Some(ranking) => {
-        if(Persist.save(ranking, force))
-          log("saved. " + paramString)
-        else
-          log("skip. already saved. " + paramString)
-      }
+  def fetchRanking(songKey: String) = {
+    val paramString = format("songKey: %s", songKey)
+    fetchRankingImpl(songKey) match {
+      case Some(ranking) =>
+        Ranking.save(ranking.toRanking)
+        log("saved. " + paramString)
+        true
       case None =>
-        log("fetch or parse fail. " + paramString)
+        false
     }
+  }
+
+  def error(resp: HttpServletResponse, message: String, code: Int) {
+    log(message)
+    resp.sendError(code, message)
   }
 
   override def doGet(req: HttpServletRequest, resp: HttpServletResponse) {
     log("task header: " + headers.map(h => Option(req.getHeader(h))).filter(_.isDefined).map(_.get).mkString(","))
     Option(req.getPathInfo).map(_.stripPrefix("/").split("/")) match {
       case Some(Array("all")) => queueAllFetching
-      case Some(Array(no@noPat(), diff@diffPat())) => fetchRanking(no, diff)
+      case Some(Array(songKey)) =>
+        if (!fetchRanking(songKey))
+          error(resp, "fetch fail. songKey: " + songKey,
+                HttpServletResponse.SC_SERVICE_UNAVAILABLE)
       case _ =>
         log("invalid pathinfo. pathinfo: " + Option(req.getPathInfo).mkString)
         resp.setStatus(HttpServletResponse.SC_NO_CONTENT)
